e8a0c4f3ed6578355319732a2b676a21fda36633
sahib <44450660+POKEPOWA@users.noreply.github.com>
2019-12-18 13:47:34 -0700

    Player
    Player still in progress

---
+++ src/entity/Player.java
@@ -0,0 +1,20 @@
+package entity;
+import java.awt.event.KeyEvent;
+public class Player {
+	addMouseListener(this);
+	int currency;
+	int width=50, height=50;
+	int x=540, y=960;
+	double targetX, targetY;
+	double velocityX, velocityY;
+	double bulletPath;
+	int shotBuffer=0;
+	int speed=1;
+	int health=100;
+	public void keypressed(KeyEvent e){
+		int key=e.getKeyCode();
+		if (key==KeyEvent.VK_LEFT){
+			
+		}
+	}
+}
