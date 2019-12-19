cbd079d2da1dbfbdc26a6c73b770f0e88f8830db
sahib <44450660+POKEPOWA@users.noreply.github.com>
2019-12-19 12:36:05 -0700

    Update Player.java

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,9 +1,8 @@
 package entity;
 
-import java.awt.MouseInfo;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
+import java.awt.event.KeyEvent;
 public class Player extends Mobs implements MouseListener{
 	int currency;
 	int width=50, height=50;
@@ -11,22 +10,45 @@ public class Player extends Mobs implements MouseListener{
 	double targetX, targetY;
 	double velocityX, velocityY;
 	double bulletPath;
-	int shotBuffer = 0;
-	
-	public void shoot(){
-		if (shotBuffer == 0){
-			targetX = MouseInfo.getPointerInfo().getLocation().getX();
-			targetY = MouseInfo.getPointerInfo().getLocation().getY();
-			
-			entityManager.add(new bullet (x,y,targetX,targetY));
-			shotBuffer = 30;
+	int shotBuffer=0;
+	int speed=1;
+	int health=100;
+	public void keypressed(KeyEvent e){
+		int key=e.getKeyCode();
+		if (key==KeyEvent.VK_A){
+			changeX=-speed;
+		}
+		if (key==KeyEvent.VK_D){
+			changeX=+speed;
+		}
+		if (key==KeyEvent.VK_W){
+			changeY=+speed;
+		}
+		if (key==KeyEvent.VK_S){
+			changeY=-speed;
 		}
 	}
-	@Override
-	public void mouseClicked(MouseEvent arg0){
-		 shoot();
+	public void keyreleased(KeyEvent e){
+		int key=e.getKeyCode();
+		if (key==KeyEvent.VK_A){
+			changeX=0;
+		}
+		if (key==KeyEvent.VK_D){
+			changeX=0;
+		}
+		if (key==KeyEvent.VK_W){
+			changeY=0;
+		}
+		if (key==KeyEvent.VK_S){
+			changeY=0;
+		}
 	}
-	
+		
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
@@ -49,6 +71,14 @@ public class Player extends Mobs implements MouseListener{
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+	}
+	
+	@Override
+	public void update(){
+		x+=changeX;
+		y+=changeY;
+		if (entities.contains(enemies)){
+			
+		}
 	}
 }
