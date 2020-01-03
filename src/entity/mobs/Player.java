ef2254b73f4c8b2a5b8a8611378b38900fdd82b2
agoogaloo <57082475+agoogaloo@users.noreply.github.com>
2020-01-02 18:17:09 -0700

    got rid of the error and organized a bit of player
    I fixed it so it can aces entitymanager and stop throwing the error but it definitely needs to be split into multiple classes because it is imposibble to actually do anything in it when its this big

--- src/entity/mobs/Player.java
+++ src/entity/mobs/Player.java
@@ -1,69 +1,71 @@
 package entity.mobs;
 
+import java.awt.Graphics;
+import java.awt.MouseInfo;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import entity.EntityManager;
-
-import java.awt.MouseInfo;
-import java.awt.event.KeyEvent;
 /**
  * @author Sahib
  */
-public class Player extends Mobs implements MouseListener{
-	int currency;
-	int width=50, height=50;
-	int x=540, y=960;
-	double targetX, targetY;
-	double velocityX, velocityY;
+//inputs and such should be split into another class because it is imposible to 
+//find anything and know whats going on already and we dont even have anything in the class yet
+public class Player extends Mobs implements MouseListener {
+	int money=0;
+	int width = 50, height = 50;
+	int x = 540, y = 960;
+	int shotBuffer = 0;
+	int speed = 1;
+	int health = 100;
+
 	double bulletPath;
-	int shotBuffer=0;
-	int speed=1;
-	int health=100;
-	public void keypressed(KeyEvent e){
-		int key=e.getKeyCode();
-		if (key==KeyEvent.VK_A){
-			changeX=-speed;
+	public void keypressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_A) {
+			changeX = -speed;
 		}
-		if (key==KeyEvent.VK_D){
-			changeX=+speed;
+		if (key == KeyEvent.VK_D) {
+			changeX = +speed;
 		}
-		if (key==KeyEvent.VK_W){
-			changeY=+speed;
+		if (key == KeyEvent.VK_W) {
+			changeY = +speed;
 		}
-		if (key==KeyEvent.VK_S){
-			changeY=-speed;
+		if (key == KeyEvent.VK_S) {
+			changeY = -speed;
 		}
 	}
-	public void keyreleased(KeyEvent e){
-		int key=e.getKeyCode();
-		if (key==KeyEvent.VK_A){
-			changeX=0;
+
+	public void keyreleased(KeyEvent e) {
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_A) {
+			changeX = 0;
 		}
-		if (key==KeyEvent.VK_D){
-			changeX=0;
+		if (key == KeyEvent.VK_D) {
+			changeX = 0;
 		}
-		if (key==KeyEvent.VK_W){
-			changeY=0;
+		if (key == KeyEvent.VK_W) {
+			changeY = 0;
 		}
-		if (key==KeyEvent.VK_S){
-			changeY=0;
+		if (key == KeyEvent.VK_S) {
+			changeY = 0;
 		}
 	}
-	
+
 	/**
 	 * @author Kevin Tea
 	 */
-	public void shoot(){
-		if (shotBuffer == 0){
+	public void shoot() {
+		if (shotBuffer == 0) {
+			double targetX, targetY;
 			targetX = MouseInfo.getPointerInfo().getLocation().getX();
 			targetY = MouseInfo.getPointerInfo().getLocation().getY();
-			
-			EntityManager.addEntity(new Bullet (x,y,targetX,targetY));
+
+			entityManager.addEntity(new Bullet(x, y, targetX, targetY));
 			shotBuffer = 30;
 		}
 	}
-		
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		shoot();
@@ -72,30 +74,30 @@ public class Player extends Mobs implements MouseListener{
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 	}
-	
+
 	@Override
-	public void update(){
-		x+=changeX;
-		y+=changeY;
+	public void update() {
+		x += changeX;
+		y += changeY;
 //		if (EntityManager.getEntities().contains(Bullet)){
 //			health-=2;
 //		}
@@ -103,11 +105,11 @@ public class Player extends Mobs implements MouseListener{
 //			health-=1;
 //		}
 		shoot();
-		shotBuffer-=1;
+		shotBuffer -= 1;
 	}
-	
-//	@Override
-//	public void render() {
-//		
-//	}
+
+	@Override
+	public void render(Graphics g) {
+
+	}
 }
