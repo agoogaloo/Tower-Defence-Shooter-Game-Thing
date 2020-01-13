a8cea39db7518e005e65d9794c97def4719cbcd4
agoogaloo <57082475+agoogaloo@users.noreply.github.com>
2020-01-12 18:07:41 -0700

    made the wall collision iff statement smaller

--- src/entity/mobs/Mobs.java
+++ src/entity/mobs/Mobs.java
@@ -8,21 +8,13 @@ public abstract class Mobs extends Entity{
 	
 	@Override
 	public void move() {
-		if(Main.getWindow().getDisplay().getFloor().checkwall((x+changeX)/16,(y+changeY)/16)){
-			changeX=0;
-			changeY=0;
-		}
-		if(Main.getWindow().getDisplay().getFloor().checkwall((x+width+changeX)/16,(y+changeY)/16)){
-			changeX=0;
-			changeY=0;
-		}
-		if(Main.getWindow().getDisplay().getFloor().checkwall((x+changeX)/16,(y+height+changeY)/16)){
-			changeX=0;
-			changeY=0;
-		}
-		if(Main.getWindow().getDisplay().getFloor().checkwall((x+width+changeX)/16,(y+height+changeY)/16)){
-			changeX=0;
-			changeY=0;
+		//not moving if the players coreners will be inside of a wall
+		if (Main.getWindow().getDisplay().getFloor().checkwall((x + changeX) / 16, (y + changeY) / 16)
+				|| Main.getWindow().getDisplay().getFloor().checkwall((x + 16 + changeX) / 16, (y + changeY) / 16)
+				|| Main.getWindow().getDisplay().getFloor().checkwall((x + changeX) / 16, (y + 29 + changeY) / 16)
+				|| Main.getWindow().getDisplay().getFloor().checkwall((x + 16 + changeX) / 16, (y + 29 + changeY) / 16)) {
+			changeX = 0;
+			changeY = 0;
 		}
 		x += changeX;// actually moving the player
 		y += changeY;
