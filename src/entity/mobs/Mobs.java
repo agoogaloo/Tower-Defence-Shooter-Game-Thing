2a86f222faa03e9faacaa4b3046d9e3aa3b40974
agoogaloo <57082475+agoogaloo@users.noreply.github.com>
2020-01-12 21:57:57 -0700

    added class decriptions

--- src/entity/mobs/Mobs.java
+++ src/entity/mobs/Mobs.java
@@ -6,11 +6,14 @@ import entity.Entity;
 //@author Matthew (updateBounds)
 //@author Sahib (move() also known as wall collision)
 public abstract class Mobs extends Entity{
+	/*
+	 * this is all the entities that move(so most of them)unlike static entities
+	 */
 	protected int speed=1, changeX, changeY;
 	
 	@Override
 	public void move() {
-		//not moving if the players coreners will be inside of a wall
+		//not moving if the players corners will be inside of a wall
 		if (Main.getWindow().getDisplay().getFloor().checkwall((x + changeX) / 16, (y + changeY) / 16)
 				|| Main.getWindow().getDisplay().getFloor().checkwall((x + 16 + changeX) / 16, (y + changeY) / 16)
 				|| Main.getWindow().getDisplay().getFloor().checkwall((x + changeX) / 16, (y + 29 + changeY) / 16)
--- src/entity/statics/Statics.java
+++ src/entity/statics/Statics.java
@@ -4,6 +4,9 @@ import entity.Entity;
 
 // @author Matthew Milum
 public abstract class Statics extends Entity {
+	/*
+	 *this is for entities that never move such as the core, heath, or money drops
+	 */
 	@Override
 	public void move() {
 		//static entities need the move method but can't move
