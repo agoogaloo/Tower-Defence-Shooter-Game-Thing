94198f6b7887da2229bc11b304df31893d4d9602
agoogaloo <57082475+agoogaloo@users.noreply.github.com>
2020-07-17 17:19:41 -0600

    improved heliBots movement more
    made heiBot go on the oposite sided as the enemy wave so you will preobably end up at all the enemies when avoiding its bullets (uses the core instead of enemy wave to make testing it easier)

--- src/entity/mobs/enemy/HeliBot.java
+++ src/entity/mobs/enemy/HeliBot.java
@@ -5,9 +5,10 @@ import java.awt.Graphics;
 import graphics.Animation;
 import graphics.Assets;
 import graphics.Camera;
+import states.GameState;
 
 public class HeliBot extends Enemy  {
-	private final int TARGETDISTANCE=65;
+	private final int TARGETDISTANCE=70;
 	
 	
 	public HeliBot(int x, int y, char direction) {
@@ -77,21 +78,33 @@ public class HeliBot extends Enemy  {
 	
 	//this is would be called move but i cant because it is used for something else
 	private void target() {
-		//getting 
-		double angle = Math.atan2(entityManager.getPlayer().getY()-y, entityManager.getPlayer().getX()-x);
-		int xMove = (int)(speed*Math.cos(angle)); //Using the bullet path angle calculates the direction and speed needed to move along the x axis
-		int yMove = (int)(speed*Math.sin(angle)); //Using the bullet path angle calculates the direction and speed needed to move along the Y axis
-		double distance=Math.sqrt(Math.pow(Math.abs(entityManager.getPlayer().getX()-x),2)+
-				Math.pow(Math.abs(entityManager.getPlayer().getY()-y),2));
-		if(distance>TARGETDISTANCE+5){
+		//the location of the enemy wave that it is trying to send the player to
+		int waveX= GameState.getFloor().ROOMSIZE*GameState.getFloor().TILESIZE*GameState.getFloor().getSize()+230,
+				waveY=GameState.getFloor().ROOMSIZE*GameState.getFloor().TILESIZE*GameState.getFloor().getSize()-248;
+		//the angle of the wave to the player 
+		double waveAngle=Math.atan2(entityManager.getPlayer().getY()-waveY, entityManager.getPlayer().getX()-waveX);
+		
+		//the point that the enemy is trying to get to. it will be TargetDistance away from the player at the 
+		//angle of wave angle.
+		int targetX= entityManager.getPlayer().getX()+(int)(TARGETDISTANCE*Math.cos(waveAngle));
+		int targetY=entityManager.getPlayer().getY()+(int)(TARGETDISTANCE*Math.sin(waveAngle));
+		
+		//getting the angle that it needs to move to reach target x/y
+		double moveAngle = Math.atan2(targetY-y, targetX-x);
+		int xMove = (int)(speed*Math.cos(moveAngle)); //how much it should move left/right
+		int yMove = (int)(speed*Math.sin(moveAngle)); //how much it should move up/down
+		
+		//how far it currently is from its targeted point
+		double offset=Math.sqrt(Math.pow(Math.abs(targetX-x),2)+
+				Math.pow(Math.abs(targetY-y),2));
+		
+		//moving towards the point if it is more than 5 pixels away from it
+		if(offset>7.5){
 			x+=xMove;
 			y+=yMove;
-		}else if(distance<TARGETDISTANCE-5){
-			x-=xMove;
-			y-=yMove;
-			
 		}
 	}
+	
 	
 	@Override
 	public void render(Graphics g, Camera camera) {
